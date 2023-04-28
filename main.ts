089245796d50a66d1d45ee9fa56ea83054fd365c
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-04-28 16:56:01 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,69 +1,12 @@
-let d = 0
+let b = 0
 basic.forever(function () {
-    d = input.compassHeading()
-    if (Math.abs(d - 45) <= 22) {
-        basic.showLeds(`
-            # . . . .
-            . # . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (Math.abs(d - 90) <= 22) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (Math.abs(d - 135) <= 22) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . # . . .
-            # . . . .
-            `)
-    } else if (Math.abs(d - 180) <= 22) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
-    } else if (Math.abs(d - 225) <= 22) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . # .
-            . . . . #
-            `)
-    } else if (Math.abs(d - 270) <= 22) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # # #
-            . . . . .
-            . . . . .
-            `)
-    } else if (Math.abs(d - 315) <= 22) {
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    }
+    b = 255 - input.lightLevel()
+    led.setBrightness(b)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
